b1cd0b7355ec83b433db72595be2ab97bcd1a287
Brad Zacher <brad.zacher@gmail.com>
2022-08-31 00:40:48 +0930

    fix lint

--- packages/website-eslint/src/mock/util.js
+++ packages/website-eslint/src/mock/util.js
@@ -1,7 +1,11 @@
 const util = {};
 
+/**
+ * @param {unknown} value
+ */
 util.inspect = function (value) {
   return value;
 };
 
+// eslint-disable-next-line import/no-default-export
 export default util;
